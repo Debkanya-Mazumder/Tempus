fc15f95c0a95953c2c1812a00d001b1de793e176
Debkanya Mazumder <dmazum2@uic.edu>
2017-09-18 16:08:32 -0500

    //calendar

--- app/src/main/java/com/nutricioapp/debkanya/tempus/StartPage.java
+++ app/src/main/java/com/nutricioapp/debkanya/tempus/StartPage.java
@@ -1,7 +1,9 @@
 package com.nutricioapp.debkanya.tempus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class StartPage extends AppCompatActivity {
@@ -16,5 +18,15 @@ public class StartPage extends AppCompatActivity {
         btncalender = (Button)findViewById(R.id.btn_startpg_calendar);
         btntrack = (Button)findViewById(R.id.btn_startpg_track);
         btnlists = (Button)findViewById(R.id.btn_startpg_lists);
+
+
+        btncalender.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent startCalendar = new Intent(getApplicationContext(), Calendar.class);
+                startActivity(startCalendar);
+            }
+        });
+
+
     }
 }
