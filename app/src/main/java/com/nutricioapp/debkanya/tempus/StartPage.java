e8d1a6090c38ecf032d8f7d45a176fef7861006d
Debkanya Mazumder <dmazum2@uic.edu>
2017-09-18 13:59:03 -0500

    //functionality for calendar, track and lists (1)

--- app/src/main/java/com/nutricioapp/debkanya/tempus/StartPage.java
+++ app/src/main/java/com/nutricioapp/debkanya/tempus/StartPage.java
@@ -2,12 +2,19 @@ package com.nutricioapp.debkanya.tempus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class StartPage extends AppCompatActivity {
 
+    Button btncalender, btnlists, btntrack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
+
+        //initializing the three buttons: calendar, track, lists.
+        btncalender = (Button)findViewById(R.id.btn_startpg_calendar);
+        btntrack = (Button)findViewById(R.id.btn_startpg_track);
+        btnlists = (Button)findViewById(R.id.btn_startpg_lists);
     }
 }
