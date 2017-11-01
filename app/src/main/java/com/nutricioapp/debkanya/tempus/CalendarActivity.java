06f25332238a1f2cf6a08b8be9b8da5c8712323d
Nithyanandh Mahalingam <nithyanandhangusa@gmail.com>
2017-11-01 05:08:08 -0500

    Fixed and added "Quit" button. Also fixed the home page logo image problem. Will continue to work on the other pages

--- app/src/main/java/com/nutricioapp/debkanya/tempus/CalendarActivity.java
+++ app/src/main/java/com/nutricioapp/debkanya/tempus/CalendarActivity.java
@@ -6,20 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.CalendarView;
-import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-public class Calendar extends AppCompatActivity {
+public class CalendarActivity extends AppCompatActivity {
 
-    private static final String TAG = "Calendar";
+    private static final String TAG = "CalendarActivity";
 
     private CalendarView mCalendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar2);
-
-
         mCalendarView  = (CalendarView) findViewById(R.id.calendarView);
         mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener()
         {
@@ -28,7 +25,6 @@ public class Calendar extends AppCompatActivity {
                 String date = (month + 1) + "/"  + day + "/" + year;
                 Log.d("calendar:",date);
                 addCalendarEvent();
-
             }
         });
     }
@@ -39,11 +35,12 @@ public class Calendar extends AppCompatActivity {
         calIntent.putExtra(CalendarContract.Events.TITLE, "Add title here");
         calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Add location");
         calIntent.putExtra(CalendarContract.Events.DESCRIPTION, "Description");
-
         GregorianCalendar calDate = new GregorianCalendar(2012, 7, 15);
         calIntent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
+
         calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
                 calDate.getTimeInMillis());
+
         calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
                 calDate.getTimeInMillis());
 
--- app/src/main/java/com/nutricioapp/debkanya/tempus/StartPage.java
+++ app/src/main/java/com/nutricioapp/debkanya/tempus/StartPage.java
@@ -1,5 +1,7 @@
 package com.nutricioapp.debkanya.tempus;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,12 +11,13 @@ import android.widget.Button;
 public class StartPage extends AppCompatActivity {
 
     Button btncalender, btnlists, btntrack, btnquit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
 
-        //initializing the three buttons: calendar, track, lists.
+        //initializing the four buttons: calendar, track, lists and quit.
         btncalender = (Button)findViewById(R.id.btn_startpg_calendar);
         btntrack = (Button)findViewById(R.id.btn_startpg_track);
         btnlists = (Button)findViewById(R.id.btn_startpg_lists);
@@ -23,7 +26,7 @@ public class StartPage extends AppCompatActivity {
         // Calander Functionality:
         btncalender.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent startCalendarButton = new Intent(getApplicationContext(), Calendar.class);
+                Intent startCalendarButton = new Intent(getApplicationContext(), CalendarActivity.class);
                 startActivity(startCalendarButton);
             }
         });
@@ -36,12 +39,40 @@ public class StartPage extends AppCompatActivity {
             }
         });
 
-        // Quit Functionality:
+        // Exit Functionality:
         btnquit.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                Intent startQuitButton = new Intent(getApplicationContext(), Quit.class);
+            public void onClick(View v) {
+                Intent startQuitButton = new Intent(Intent.ACTION_MAIN);
+                startQuitButton.addCategory(Intent.CATEGORY_HOME);
+                startQuitButton.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(startQuitButton);
             }
+                /* ------------------------ Back button on home page - to exit functionality yet to be implemented ----------------------
+
+                public void onBackPressed()
+                {
+                    // On pressing the 'back' button to exit the app,,the alert dialog box pop's up:
+                    AlertDialog.Builder builder = new AlertDialog.Builder(StartPage.this);
+                    builder.setMessage("Do you intend to exit Tempus?");
+                    builder.setCancelable(true);
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            finish();
+                        }
+                    });
+                    builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
+            });
+        }
+        */
         });
     }
-};
+}
